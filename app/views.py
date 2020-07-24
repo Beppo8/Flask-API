49b7d43bb03ad4acd4b21063b9ab35729289b4c7
Beppo <beppo@localhost.localdomain>
2020-07-24 17:38:38 -0500

    Decorator and Marshmallow

--- app/views.py
+++ app/views.py
@@ -4,7 +4,23 @@ from flask import Blueprint
 from .models.task import Task
 from .responses import response, not_found, bad_request
 
+from .schemas import task_schema, tasks_schema
+from .schemas import params_tasks_schema
+
 api_v1 = Blueprint('api', __name__, url_prefix='/api/v1')
+
+def set_task(function):
+    def wrap(*args, **kwargs):
+        id = kwargs.get('id', 0)
+        task = Task.query.filter_by(id=id).first()
+
+        if task is None:
+            return not_found()
+
+        return function(task)
+
+    wrap.__name__ = function.__name__
+    return wrap
 
 @api_v1.route('/tasks', methods=['GET'])
 def get_tasks():
@@ -13,46 +29,31 @@ def get_tasks():
 
     tasks = Task.get_by_page(order, page)
 
-    return response([
-        task.serialize() for task in tasks
-    ])
+    return response(tasks_schema.dump(tasks))
 
 @api_v1.route('/tasks/<id>', methods=['GET'])
-def get_task(id):
-    
-    task = Task.query.filter_by(id=id).first()
-
-    if task is None:
-        return not_found()
-
-    return response(task.serialize())
+@set_task
+def get_task(task):
+    return response(task_schema.dump(task))
 
 @api_v1.route('/tasks', methods=['POST'])
 def create_task():
     json = request.get_json(force=True)
 
-    if json.get('title') is None or len(json['title']) > 50:
-        return bad_request()
-
-    if json.get('description') is None:
-        return bad_request()
-
-    if json.get('deadline') is None:
+    error = params_tasks_schema.validate(json)
+    if erro:
+        print(error)
         return bad_request()
 
     task = Task.new(json['title'], json['description'], json['deadline'])
     if task.save():
-        return response(task.serialize())
+        return response(task_schema.dump(task))
 
     return bad_request()
 
 @api_v1.route('/tasks/<id>', methods=['PUT'])
-def update_task(id):
-    task = Task.query.filter_by(id=id).first()
-
-    if task is None:
-        return not_found()
-
+@set_task
+def update_task(task):
     json = request.get_json(force=True)
 
     task.title = json.get('title', task.title)
@@ -60,18 +61,14 @@ def update_task(id):
     task.deadline = json.get('deadline', task.deadline)
 
     if task.save():
-        return response(task.serialize())
+        return response(task_schema.dump(task))
 
     return bad_request()
 
 @api_v1.route('/tasks/<id>', methods=['DELETE'])
-def delete_task(id):
-    task = Task.query.filter_by(id=id).first()
-
-    if task is None:
-        return not_found()
-
+@set_task
+def delete_task(task):
     if task.delete():
-        return response(task.serialize())
+        return response(task_schema.dump(task))
     
     return bad_request()
